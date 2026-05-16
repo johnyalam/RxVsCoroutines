fd63cdeb8f344f5472b929431062426456738205
Johny Alam <johnyalam.lut@gmail.com>
2026-05-16 23:38:10 +0300

    HomeScreen updated with content (#5)

--- app/src/main/java/fi/developer/rxvscoroutines/presentation/ui/screen/HomeScreen.kt
+++ app/src/main/java/fi/developer/rxvscoroutines/presentation/ui/screen/HomeScreen.kt
@@ -1,46 +1,18 @@
 package fi.developer.rxvscoroutines.presentation.ui.screen
 
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import fi.developer.rxvscoroutines.presentation.ui.components.CoinItem
-import fi.developer.rxvscoroutines.presentation.ui.components.ErrorMessage
 import fi.developer.rxvscoroutines.presentation.ui.viewmodel.HomeScreenViewModel
 
 @Composable
 fun HomeScreen(viewModel: HomeScreenViewModel = hiltViewModel()) {
     val state = viewModel.state.collectAsStateWithLifecycle()
-    Column(
-        modifier = Modifier.fillMaxSize().padding(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-    ) {
-        LazyColumn {
-            items(
-                items = state.value.data,
-            ) { coin ->
-                CoinItem(coin, onItemClick = {
-                    Log.d("TAG", "Clicked!!!")
-                })
-            }
+    HomeScreenContent(
+        state = state.value,
+        onItemClick = { coin ->
+            Log.d("TAG", "Item Clicked ${coin.name}")
         }
-
-        if (state.value.error.isNotBlank()) {
-            ErrorMessage(message = state.value.error)
-        }
-        if (state.value.isLoading) {
-            CircularProgressIndicator(Modifier.align(Alignment.CenterHorizontally))
-        }
-    }
+    )
 }
---
+++ app/src/main/java/fi/developer/rxvscoroutines/presentation/ui/screen/HomeScreenContent.kt
@@ -0,0 +1,49 @@
+package fi.developer.rxvscoroutines.presentation.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
+import fi.developer.rxvscoroutines.domain.model.CoinItem
+import fi.developer.rxvscoroutines.presentation.ui.components.CoinItem
+import fi.developer.rxvscoroutines.presentation.ui.components.ErrorMessage
+import fi.developer.rxvscoroutines.presentation.ui.viewmodel.CoinListState
+
+@Composable
+fun HomeScreenContent(
+    state: CoinListState,
+    onItemClick: (CoinItem) -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+    ) {
+        LazyColumn(
+            modifier = Modifier.testTag("coin_list")
+        ) {
+            items(state.data) { coin ->
+                CoinItem(
+                    coinItem = coin,
+                    onItemClick = onItemClick
+                )
+            }
+        }
+
+        if (state.error.isNotBlank()) {
+            ErrorMessage(state.error)
+        }
+
+        if (state.isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.testTag("loading")
+            )
+        }
+    }
+}
